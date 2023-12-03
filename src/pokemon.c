c449ccb8a25ef79912244ca18ecc5c0e653922fa
rbruno22 <rbruno@libgot.com>
2023-12-03 00:03:24 -0300

    fix

--- src/pokemon.c
+++ src/pokemon.c
@@ -95,14 +95,14 @@ informacion_pokemon_t *pokemon_cargar_archivo(const char *path)
 			}
 		}
 		info->cantidad++;
-		// info->pokemones = realloc(info->pokemones,
-		// 			  sizeof(pokemon_t) * info->cantidad);
-		// if (!info->pokemones) {
-		// 	free(info);
-		// 	free(pokemon);
-		// 	fclose(archivo);
-		// 	return NULL;
-		// }
+		info->pokemones = realloc(info->pokemones,
+					  sizeof(pokemon_t) * info->cantidad);
+		if (!info->pokemones) {
+			free(info);
+			free(pokemon);
+			fclose(archivo);
+			return NULL;
+		}
 		info->pokemones[info->cantidad - 1] = *pokemon;
 	}
 
