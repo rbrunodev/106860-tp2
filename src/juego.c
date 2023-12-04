bcdda9d7459341388e047a7c02bb118596abd856
rbruno22 <rbruno@libgot.com>
2023-12-04 13:10:12 -0300

    prueba

--- src/juego.c
+++ src/juego.c
@@ -51,6 +51,7 @@ JUEGO_ESTADO juego_cargar_pokemon(juego_t *juego, char *archivo)
 
 	informacion_pokemon_t *info = pokemon_cargar_archivo(archivo);
 	if(info == NULL){ 
+		free(info);
 		return ERROR_GENERAL;
 	}
 
